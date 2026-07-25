ac661059e8299be213bf070d31ac41e5c04f38c2
kiranreddy916 <samuelkiranreddy916@gmail.com>
2026-07-25 20:31:03 +0530

    style: remove layout padding top on profile pages and refine neon glow surroundings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Onboarding from './pages/Onboarding';
 import Home from './pages/Home';
@@ -15,27 +15,36 @@ import AddByCode from './pages/AddByCode';
 import BottomNav from './components/BottomNav';
 import './App.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isProfilePage = location.pathname.startsWith('/profile') || location.pathname.startsWith('/friend-profile');
+
+  return (
+    <div className={`app-layout ${isProfilePage ? 'profile-page-layout' : ''}`}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/exercise" element={<Exercise />} />
+        <Route path="/workout-logger" element={<WorkoutLogger />} />
+        <Route path="/checklist" element={<Checklist />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/add/:code" element={<AddByCode />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/friend-profile/:id" element={<FriendProfile />} />
+        <Route path="/badges" element={<Badges />} />
+        <Route path="/prs" element={<PersonalRecords />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <BottomNav />
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-layout">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/exercise" element={<Exercise />} />
-          <Route path="/workout-logger" element={<WorkoutLogger />} />
-          <Route path="/checklist" element={<Checklist />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/add/:code" element={<AddByCode />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/friend-profile/:id" element={<FriendProfile />} />
-          <Route path="/badges" element={<Badges />} />
-          <Route path="/prs" element={<PersonalRecords />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <BottomNav />
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
